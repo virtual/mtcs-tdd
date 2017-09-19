6a3fcaf4804e947452c97c7129f3547f61cd7b73
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-19 13:41:46 -0600

    add in date check for days until

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ function barf() {
 function vomit() {
     return "vomit";
 }
+function returnDate(d) {
+    var newDate = new Date(d);
+    var todayDate = new Date();
+    var diff = Math.ceil((newDate - todayDate) 
+    / (24 * 60 * 60 * 1000));
+    return diff;
+} 
 
+if (returnDate("12/25/17") === 97) {
+    console.log('test 3 passed');
+} else {
+    console.log('test 3 failed, days until Christmas, 97');
+}
+if (returnDate("9/20/17") === 1) {
+    console.log('test 4 passed');
+} else {
+    console.log('test 4 failed');
+}
 if (vomit() === "vomit") {
     console.log('test 2 passed')
 } else {
