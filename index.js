1b60c4452a3d5d06063bfcdd6bbeee5a8910b1f8
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-19 14:02:19 -0600

    change to days til Christmas

--- index.js
+++ index.js
@@ -6,23 +6,35 @@ function vomit() {
     return "vomit";
 }
 function returnDate(d) {
-    var newDate = new Date(d);
-    var todayDate = new Date();
-    var diff = Math.ceil((newDate - todayDate) 
-    / (24 * 60 * 60 * 1000));
+    if (!d) {
+        
+        var newDate = new Date().setHours(0, 0, 0);    
+    } else {
+        var newDate = new Date(d);
+    } 
+    var christmasDate = new Date("12/25/17");
+    var diff = Math.floor((christmasDate - newDate) 
+    / (24 * 60 * 60 * 1000)); 
     return diff;
 } 
 
-if (returnDate("12/25/17") === 97) {
-    console.log('test 3 passed');
+if (returnDate() === 97) {
+    console.log('test 5 passed');
 } else {
-    console.log('test 3 failed, days until Christmas, 97');
+    console.log('test 5 failed, days until Christmas, 97');
 }
-if (returnDate("9/20/17") === 1) {
+
+if (returnDate("12/25/17") === 0) {
     console.log('test 4 passed');
 } else {
     console.log('test 4 failed');
 }
+if (returnDate("9/19/17") === 97) {
+    console.log('test 3 passed');
+} else {
+    console.log('test 3 failed, days until Christmas, 97');
+}
+
 if (vomit() === "vomit") {
     console.log('test 2 passed')
 } else {
